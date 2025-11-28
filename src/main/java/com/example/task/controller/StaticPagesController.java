f198df719f4aff25d8d9ba0dc378a94094743a64
Parto5 <patryk.piasecki@gmail.com>
2025-11-28 16:38:35 +0100

    Dodany plik tekstowy i zdjęcie dojazdu lokalnie do użycia w kontakcie i informatorze.

--- src/main/java/com/example/task/controller/StaticPagesController.java
+++ src/main/java/com/example/task/controller/StaticPagesController.java
@@ -1,13 +1,24 @@
 package com.example.task.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @Controller
 public class StaticPagesController {
 
     @GetMapping("/o-przychodni")
-    public String about() {
+    public String about(Model model) throws
+            IOException {
+        Path path = Paths.get("src/main/resources/static/data/o-przychodni.txt");
+        List<String> lines = Files.readAllLines(path);
+        model.addAttribute("infoLines", lines);
         return "o-przychodni";
     }
 
